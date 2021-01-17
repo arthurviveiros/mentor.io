b669717ad789f20069e85c52898bb48c2cdaaf52
Arthur Viveiros <arthur.viveiros@7comm.com.br>
2021-01-17 19:59:48 -0300

    Criação do Home e inicio do Discovery

--- Mentorio/src/app/app-routing.module.ts
+++ Mentorio/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { DiscoveryComponent } from './pages/discovery/discovery.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '', component: HomeComponent, children: [
+      { path: 'home', component: FeedComponent },
+      { path: 'discovery', component: DiscoveryComponent},
+      { path: 'add', component: AddComponent},
+      { path: 'chat', component: ChatComponent},
+      { path: 'profile', component: ProfileComponent}
+    ]
+  }
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Mentorio/src/app/app.module.ts
+++ Mentorio/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedComponent } from './pages/feed/feed.component';
+import { DiscoveryComponent } from './pages/discovery/discovery.component';
+import { AddComponent } from './pages/add/add.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FeedComponent,
+    DiscoveryComponent,
+    AddComponent,
+    ChatComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
